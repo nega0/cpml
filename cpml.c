b78700c2ff06e41fc65456f75dbb7e8f85fb3dd0
nega <nega@maru>
2011-10-05 16:44:37 -0400

    more minor formatting changes

--- cpml.c
+++ cpml.c
@@ -30,19 +30,19 @@ struct arithmetic {
 		char *	library;
 	} implementation[12];
 } function[] = {
-	{ "acos",  { -.99, -1 },  { { "acos",  "libm.so" }, { "amd_acos",  "libamdlibm.so" }, 0x00 } }, //{ "acos", "libcpml.so" }, { "F_acos", "libcpml.so"}, { "acos", "libimf.so" }, 0x00 } },
-	{ "asin",  { .99, -1 },   { { "asin",  "libm.so" }, { "amd_asin",  "libamdlibm.so" }, 0x00 } }, //{ "asin", "libcpml.so" }, { "F_asin", "libcpml.so"}, { "asin", "libimf.so" }, 0x00 } },
-	{ "atan",  { 200., -1 },  { { "atan",  "libm.so" }, { "amd_atan",  "libamdlibm.so" }, 0x00 } }, //{ "atan", "libcpml.so" }, { "F_atan", "libcpml.so"}, { "atan", "libimf.so" }, 0x00 } },
-	{ "atan2", { 1.1, 2.1 },  { { "atan2", "libm.so" }, { "amd_atan2", "libamdlibm.so" }, 0x00 } }, //{ "atan2", "libcpml.so" }, { "F_atan2", "libcpml.so"}, { "atan2", "libimf.so" }, 0x00 } },
-	{ "cos",   { 1.57, -1 },  { { "cos",   "libm.so" }, { "amd_cos",   "libamdlibm.so" }, 0x00 } }, //{ "cos", "libcpml.so" }, { "F_cos", "libcpml.so" }, { "cos", "libimf.so" }, 0x00 } },
-	{ "exp",   { 2.1, -1 },   { { "exp",   "libm.so" }, { "amd_exp",   "libamdlibm.so" }, 0x00 } }, //{ "exp", "libcpml.so" }, { "F_exp", "libcpml.so" }, { "exp", "libimf.so" }, 0x00 } },
+	{ "acos",  { -.99, -1 },  { { "acos",  "libm.so" }, { "amd_acos",  "libamdlibm.so" }, 0x00 } }, //{ "acos",  "libcpml.so" }, { "F_acos",  "libcpml.so" }, { "acos",  "libimf.so" }, 0x00 } },
+	{ "asin",  { .99, -1 },   { { "asin",  "libm.so" }, { "amd_asin",  "libamdlibm.so" }, 0x00 } }, //{ "asin",  "libcpml.so" }, { "F_asin",  "libcpml.so" }, { "asin",  "libimf.so" }, 0x00 } },
+	{ "atan",  { 200., -1 },  { { "atan",  "libm.so" }, { "amd_atan",  "libamdlibm.so" }, 0x00 } }, //{ "atan",  "libcpml.so" }, { "F_atan",  "libcpml.so" }, { "atan",  "libimf.so" }, 0x00 } },
+	{ "atan2", { 1.1, 2.1 },  { { "atan2", "libm.so" }, { "amd_atan2", "libamdlibm.so" }, 0x00 } }, //{ "atan2", "libcpml.so" }, { "F_atan2", "libcpml.so" }, { "atan2", "libimf.so" }, 0x00 } },
+	{ "cos",   { 1.57, -1 },  { { "cos",   "libm.so" }, { "amd_cos",   "libamdlibm.so" }, 0x00 } }, //{ "cos",   "libcpml.so" }, { "F_cos",   "libcpml.so" }, { "cos",   "libimf.so" }, 0x00 } },
+	{ "exp",   { 2.1, -1 },   { { "exp",   "libm.so" }, { "amd_exp",   "libamdlibm.so" }, 0x00 } }, //{ "exp",   "libcpml.so" }, { "F_exp",   "libcpml.so" }, { "exp",   "libimf.so" }, 0x00 } },
 	{ "hypot", { 5.0, 5.0 },  { { "hypot", "libm.so" }, { "amd_hypot", "libamdlibm.so" }, 0x00 } }, //{ "hypot", "libcpml.so" }, { "F_hypot", "libcpml.so" }, { "hypot", "libimf.so" }, 0x00 } },
-	{ "log",   { 2.1, -1 },   { { "log",   "libm.so" }, { "amd_log",   "libamdlibm.so" }, 0x00 } }, //{ "log", "libcpml.so" }, { "F_log", "libcpml.so" }, { "log", "libimf.so" }, 0x00 } },
+	{ "log",   { 2.1, -1 },   { { "log",   "libm.so" }, { "amd_log",   "libamdlibm.so" }, 0x00 } }, //{ "log",   "libcpml.so" }, { "F_log",   "libcpml.so" }, { "log",   "libimf.so" }, 0x00 } },
 	{ "log10", { 2.1, -1 },   { { "log10", "libm.so" }, { "amd_log10", "libamdlibm.so" }, 0x00 } }, //{ "log10", "libcpml.so" }, { "F_log10", "libcpml.so" }, { "log10", "libimf.so" }, 0x00 } },
-	{ "pow",   { 1.1, 10.1 }, { { "pow",   "libm.so" }, { "amd_pow",   "libamdlibm.so" }, 0x00 } }, //{ "pow", "libcpml.so" }, { "F_pow", "libcpml.so" }, { "pow", "libimf.so" }, 0x00 } },
-	{ "sin",   { 1.96, -1 },  { { "sin",   "libm.so" }, { "amd_sin",   "libamdlibm.so" }, 0x00 } }, //{ "sin", "libcpml.so" }, { "F_sin", "libcpml.so" }, { "sin", "libimf.so" }, 0x00 } },
-	{ "sqrt",  { 10.0, -1 },  { { "sqrt",  "libm.so" }, { "amd_sqrt",  "libamdlibm.so" }, 0x00 } }, //{ "sqrt", "libcpml.so" }, { "F_sqrt", "libcpml.so"}, { "sqrt", "libimf.so" }, 0x00 } },
-	{ "tan",   { 7.85, -1 },  { { "tan",   "libm.so" }, { "amd_tan",   "libamdlibm.so" }, 0x00 } }, //{ "tan", "libcpml.so" }, { "F_tan", "libcpml.so" }, { "tan", "libimf.so" }, 0x00 } },	
+	{ "pow",   { 1.1, 10.1 }, { { "pow",   "libm.so" }, { "amd_pow",   "libamdlibm.so" }, 0x00 } }, //{ "pow",   "libcpml.so" }, { "F_pow",   "libcpml.so" }, { "pow",   "libimf.so" }, 0x00 } },
+	{ "sin",   { 1.96, -1 },  { { "sin",   "libm.so" }, { "amd_sin",   "libamdlibm.so" }, 0x00 } }, //{ "sin",   "libcpml.so" }, { "F_sin",   "libcpml.so" }, { "sin",   "libimf.so" }, 0x00 } },
+	{ "sqrt",  { 10.0, -1 },  { { "sqrt",  "libm.so" }, { "amd_sqrt",  "libamdlibm.so" }, 0x00 } }, //{ "sqrt",  "libcpml.so" }, { "F_sqrt",  "libcpml.so" }, { "sqrt",  "libimf.so" }, 0x00 } },
+	{ "tan",   { 7.85, -1 },  { { "tan",   "libm.so" }, { "amd_tan",   "libamdlibm.so" }, 0x00 } }, //{ "tan",   "libcpml.so" }, { "F_tan",   "libcpml.so" }, { "tan",   "libimf.so" }, 0x00 } },
 	0x00
 };
 
