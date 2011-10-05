71caf3a6a3f62ad08868fc3fcd2d3449a4802fe1
nega <nega@maru>
2011-10-05 14:53:35 -0400

    64bit-ify the x86 asm

--- Makefile
+++ Makefile
@@ -1,10 +1,18 @@
+AMDLIBM_DIR=/opt/amdlibm-3-0-1-lin64/lib/dynamic
+
 CC= gcc
 LIBS= -ldl
 
 OBJS= cpml.o
 
 cpml: $(OBJS)
-	gcc -o $@ $^ $(CFLAGS) $(LIBS)
+	gcc -m64 -o $@ $^ $(CFLAGS) $(LIBS)
+
+run: cpml
+	LD_LIBRARY_PATH=$(AMDLIBM_DIR) ./cpml
+
+cmp: cpml 
+	LD_LIBRARY_PATH=$(AMDLIBM_DIR) ./cpml | perl rfmt.pl
 
 .PHONY: clean
 
--- cpml.c
+++ cpml.c
@@ -1,9 +1,11 @@
+/* -*- mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8; -*- */
 #define _GNU_SOURCE
 
 #include <stdio.h>
 #include <unistd.h>
 #include <dlfcn.h>
 #include <signal.h>
+#include <stdint.h>
 
 /* 
  * benchmark different double precision maths functions.
@@ -28,21 +30,31 @@ struct arithmetic {
 		char *	library;
 	} implementation[12];
 } function[] = {
-	{ "acos", { -.99, -1 }, { { "acos", "libm.so"}, { "acos", "libcpml.so" }, { "F_acos", "libcpml.so"}, { "acos", "libimf.so" }, 0x00 } },
-	{ "asin", { .99, -1 }, { { "asin", "libm.so"}, { "asin", "libcpml.so" }, { "F_asin", "libcpml.so"}, { "asin", "libimf.so" }, 0x00 } },
-	{ "atan", { 200., -1 }, { { "atan", "libm.so"}, { "atan", "libcpml.so" }, { "F_atan", "libcpml.so"}, { "atan", "libimf.so" }, 0x00 } },
-	{ "atan2", { 1.1, 2.1 }, { { "atan2", "libm.so"}, { "atan2", "libcpml.so" }, { "F_atan2", "libcpml.so"}, { "atan2", "libimf.so" }, 0x00 } },
-	{ "cos", { 1.57, -1 }, { { "cos", "libm.so" }, { "cos", "libcpml.so" }, { "F_cos", "libcpml.so" }, { "cos", "libimf.so" }, 0x00 } }, 
-	{ "exp", { 2.1, -1 },  { { "exp", "libm.so" }, { "exp", "libcpml.so" }, { "F_exp", "libcpml.so" }, { "exp", "libimf.so" }, 0x00 } },
-	{ "hypot", { 5.0, 5.0 },  { { "hypot", "libm.so" }, { "hypot", "libcpml.so" }, { "F_hypot", "libcpml.so" }, { "hypot", "libimf.so" }, 0x00 } },
-	{ "log", { 2.1, -1 },  { { "log", "libm.so" }, { "log", "libcpml.so" }, { "F_log", "libcpml.so" }, { "log", "libimf.so" }, 0x00 } },
-	{ "log10", { 2.1, -1 },  { { "log10", "libm.so" }, { "log10", "libcpml.so" }, { "F_log10", "libcpml.so" }, { "log10", "libimf.so" }, 0x00 } },
-	{ "pow", { 1.1, 10.1 }, { { "pow", "libm.so" }, { "pow", "libcpml.so" }, { "F_pow", "libcpml.so" }, { "pow", "libimf.so" }, 0x00 } },
-	{ "sin", { 1.96, -1 }, { { "sin", "libm.so" }, { "sin", "libcpml.so" }, { "F_sin", "libcpml.so" }, { "sin", "libimf.so" }, 0x00 } },
-	{ "sqrt", { 10.0, -1 }, { { "sqrt", "libm.so"}, { "sqrt", "libcpml.so" }, { "F_sqrt", "libcpml.so"}, { "sqrt", "libimf.so" }, 0x00 } },
-	{ "tan", {7.85, -1 }, { { "tan", "libm.so" }, { "tan", "libcpml.so" }, { "F_tan", "libcpml.so" }, { "tan", "libimf.so" }, 0x00 } },
+	{ "acos",  { -.99, -1 },  { { "acos",  "libm.so"},  { "amd_acos",  "libamdlibm.so" }, 0x00 } }, //{ "acos", "libcpml.so" }, { "F_acos", "libcpml.so"}, { "acos", "libimf.so" }, 0x00 } },
+	{ "asin",  { .99, -1 },   { { "asin",  "libm.so"},  { "amd_asin",  "libamdlibm.so" }, 0x00 } }, //{ "asin", "libcpml.so" }, { "F_asin", "libcpml.so"}, { "asin", "libimf.so" }, 0x00 } },
+	{ "atan",  { 200., -1 },  { { "atan",  "libm.so"},  { "amd_atan",  "libamdlibm.so" }, 0x00 } }, //{ "atan", "libcpml.so" }, { "F_atan", "libcpml.so"}, { "atan", "libimf.so" }, 0x00 } },
+	{ "atan2", { 1.1, 2.1 },  { { "atan2", "libm.so"},  { "amd_atan2", "libamdlibm.so" }, 0x00 } }, //{ "atan2", "libcpml.so" }, { "F_atan2", "libcpml.so"}, { "atan2", "libimf.so" }, 0x00 } },
+	{ "cos",   { 1.57, -1 },  { { "cos",   "libm.so" }, { "amd_cos",   "libamdlibm.so" }, 0x00 } }, //{ "cos", "libcpml.so" }, { "F_cos", "libcpml.so" }, { "cos", "libimf.so" }, 0x00 } },
+	{ "exp",   { 2.1, -1 },   { { "exp",   "libm.so" }, { "amd_exp",   "libamdlibm.so" }, 0x00 } }, //{ "exp", "libcpml.so" }, { "F_exp", "libcpml.so" }, { "exp", "libimf.so" }, 0x00 } },
+	{ "hypot", { 5.0, 5.0 },  { { "hypot", "libm.so" }, { "amd_hypot", "libamdlibm.so" }, 0x00 } }, //{ "hypot", "libcpml.so" }, { "F_hypot", "libcpml.so" }, { "hypot", "libimf.so" }, 0x00 } },
+	{ "log",   { 2.1, -1 },   { { "log",   "libm.so" }, { "amd_log",   "libamdlibm.so" }, 0x00 } }, //{ "log", "libcpml.so" }, { "F_log", "libcpml.so" }, { "log", "libimf.so" }, 0x00 } },
+	{ "log10", { 2.1, -1 },   { { "log10", "libm.so" }, { "amd_log10", "libamdlibm.so" }, 0x00 } }, //{ "log10", "libcpml.so" }, { "F_log10", "libcpml.so" }, { "log10", "libimf.so" }, 0x00 } },
+	{ "pow",   { 1.1, 10.1 }, { { "pow",   "libm.so" }, { "amd_pow",   "libamdlibm.so" }, 0x00 } }, //{ "pow", "libcpml.so" }, { "F_pow", "libcpml.so" }, { "pow", "libimf.so" }, 0x00 } },
+	{ "sin",   { 1.96, -1 },  { { "sin",   "libm.so" }, { "amd_sin",   "libamdlibm.so" }, 0x00 } }, //{ "sin", "libcpml.so" }, { "F_sin", "libcpml.so" }, { "sin", "libimf.so" }, 0x00 } },
+	{ "sqrt",  { 10.0, -1 },  { { "sqrt",  "libm.so"},  { "amd_sqrt",  "libamdlibm.so" }, 0x00 } }, //{ "sqrt", "libcpml.so" }, { "F_sqrt", "libcpml.so"}, { "sqrt", "libimf.so" }, 0x00 } },
+	{ "tan",   {7.85, -1 },   { { "tan",   "libm.so" }, { "amd_tan",   "libamdlibm.so" }, 0x00 } }, //{ "tan", "libcpml.so" }, { "F_tan", "libcpml.so" }, { "tan", "libimf.so" }, 0x00 } },
 	0x00
 };
+
+#ifndef __alpha__
+__inline__ uint64_t rdtsc(void) {
+	uint32_t l, h;
+	__asm__ __volatile__ ("xorl %%eax,%%eax\ncpuid"
+			      ::: "%rax", "%rbx", "%rcx", "%rdx");
+	__asm__ __volatile__ ("rdtsc" : "=a" (l), "=d" (h));
+	return (uint64_t)h << 32 | l;
+}
+#endif
 
 int main (void)
 {
@@ -60,7 +72,7 @@ int main (void)
 	(void) nice (-20); /* this will only work for uid 0 */
 	functionp = function;
 	while (functionp->standard) {
-		fprintf (stderr, "\n%s:\n", functionp->standard);
+		fprintf (stdout, "%s:\t", functionp->standard);
 		implementationp = functionp->implementation;
 		while (implementationp->name) {
 			if (!(library = dlopen (implementationp->library, RTLD_NOW))) {
@@ -85,7 +97,7 @@ int main (void)
 #ifdef __alpha__
 			__asm__ __volatile__ ("rpcc %0" : "=r" (start));
 #else
-			__asm__ __volatile__ ("rdtsc"   : "=A" (start));
+			start = rdtsc();
 #endif
 
 #if 0
@@ -102,15 +114,17 @@ int main (void)
 #ifdef __alpha__
 			__asm__ __volatile__ ("rpcc %0" : "=r" (end));
 #else
-			__asm__ __volatile__ ("rdtsc"   : "=A" (end));
+			end = rdtsc();
 #endif
-			fprintf (stderr, "\t%10s->%s()\t(%llu cycles)\n",
-					implementationp->library, implementationp->name, 
-					(end - start)/cnt);
+			// fprintf (stderr, "\t%10s->%s()\t(%llu cycles)\n",
+			fprintf (stdout, "%s->%s()\t%llu\t",
+				 implementationp->library, implementationp->name, 
+				 (end - start)/cnt);
 
 			dlclose (library);
 			implementationp++;
 		}
+		fprintf (stdout, "\n");
 		functionp++;
 	}
 	return 0;
